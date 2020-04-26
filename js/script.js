2738f6028e82448e4d488a412465352c9d945330
johnpandit <63198475+johnpandit@users.noreply.github.com>
2020-04-26 15:39:35 -0400

    Add javascript functionality to the menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("Welcome to John's Portfolio Site!")
+function menuToggle() {
+  var x = document.getElementbyId('myNavtoggle');
+  if(x.className === 'navtoggle'){
+    x.className += 'responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
